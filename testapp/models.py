10e36f02a63d2f28f538b713b2ade506191348fc
Адилхан Бейсенбаев <@itstep.loc>
2023-08-28 17:36:55 +0600

    Модели: расширенные

--- testapp/models.py
+++ testapp/models.py
@@ -1,34 +1,24 @@
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
+
+from bboard.models import Rubric
 
 
 class AdvUser(models.Model):
-    is_activated = models.BooleanField(
-        default=True,
-    )
-
-    user = models.OneToOneField(
-        User,
-        on_delete=models.CASCADE
-    )
+    is_activated = models.BooleanField(default=True, )
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
 
 class Spare(models.Model):
-    name = models.CharField(
-        max_length=30,
-    )
+    name = models.CharField(max_length=30, )
 
 
 class Machine(models.Model):
-    name = models.CharField(
-        max_length=30,
-    )
-
-    spares = models.ManyToManyField(
-        Spare,
-        through='Kit',
-        through_fields=('machine', 'spare')
-    )
+    name = models.CharField(max_length=30, )
+    spares = models.ManyToManyField(Spare, through='Kit', through_fields=('machine', 'spare'))
+    notes = GenericRelation('Note')
 
 
 class Kit(models.Model):
@@ -38,24 +28,9 @@ class Kit(models.Model):
 
 
 class SMS(models.Model):
-    comment = models.CharField(
-        max_length=120,
-        verbose_name='Комментарий'
-    )
-
-    sender = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='sender',
-        verbose_name='Отправитель'
-    )
-
-    receiver = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name='receiver',
-        verbose_name='Получатель'
-    )
+    comment = models.CharField(max_length=120, verbose_name='Комментарий')
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sender', verbose_name='Отправитель')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='receiver', verbose_name='Получатель')
 
     def __str__(self):
         return self.comment
@@ -79,3 +54,45 @@ class Registration(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     date = models.DateTimeField()
 
+
+class Note(models.Model):
+    content = models.TextField()
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey(ct_field='content_type', fk_field='object_id')
+
+
+# Прямое наследование
+# class Message(models.Model):
+#     content = models.TextField()
+#
+#
+# class PrivateMessage(Message):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     message = models.OneToOneField(Message, on_delete=models.CASCADE, parent_link=True)
+
+
+# Абстрактные модели
+
+class Message(models.Model):
+    content = models.TextField()
+    name = models.CharField(max_length=20)
+    email = models.EmailField()
+
+    class Meta:
+        abstract = True
+        ordering = ['name']
+
+
+class PrivateMessage(Message):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=40)
+    email = None
+
+
+# Прокси-Модели
+
+class RevRubric(Rubric):
+    class Meta:
+        proxy = True
+        ordering = ['-name']
